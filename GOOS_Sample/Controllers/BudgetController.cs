c06b8ab4505c9113c45e86159f43e0716ff4e33e
Juggernaut <juggernaut.liu@gmail.com>
2017-07-01 15:05:14 +0800

    add 1 controller unit test

--- GOOS_Sample/Controllers/BudgetController.cs
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -5,11 +5,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GOOS_Sample.Models;
 
 namespace GOOS_Sample.Controllers
 {
     public class BudgetController : Controller
     {
+        private IBudgetService budgetServiceStub;
+
+        public BudgetController()
+        {
+        }
+
+        public BudgetController(IBudgetService budgetServiceStub)
+        {
+            this.budgetServiceStub = budgetServiceStub;
+        }
 
         // GET: Budget
         public ActionResult Add()
@@ -20,12 +31,15 @@ namespace GOOS_Sample.Controllers
         [HttpPost]
         public ActionResult Add(BudgetAddViewModel model)
         {
-            using (var dbcontext = new goosEntitiesForReal())
-            {
-                var budget = new Budgets() { Amount = model.Amount, YearMonth = model.Month };
-                dbcontext.Budgets.Add(budget);
-                dbcontext.SaveChanges();
-            }
+            //using (var dbcontext = new goosEntitiesForReal())
+            //{
+            //    var budget = new Budgets() { Amount = model.Amount, YearMonth = model.Month };
+            //    dbcontext.Budgets.Add(budget);
+            //    dbcontext.SaveChanges();
+            //}
+
+            this.budgetServiceStub.Create(model);
+           
             ViewBag.Message = "added successfully";
             return View(model);
         }
