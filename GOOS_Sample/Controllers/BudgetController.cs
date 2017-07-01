954649ee15b906d4e13fee9db4810c221eedbb1b
Juggernaut <juggernaut.liu@gmail.com>
2017-07-01 14:15:39 +0800

    Add real production code

--- GOOS_Sample/Controllers/BudgetController.cs
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -1,4 +1,5 @@
-﻿using GOOS_Sample.Models.ViewModels;
+﻿using GOOS_Sample.Models.dataModels;
+using GOOS_Sample.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +16,16 @@ namespace GOOS_Sample.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Add(BudgetAddViewModel model)
         {
+            using (var dbcontext = new goosEntitiesForReal())
+            {
+                var budget = new Budgets() { Amount = model.Amount, YearMonth = model.Month };
+                dbcontext.Budgets.Add(budget);
+                dbcontext.SaveChanges();
+            }
             ViewBag.Message = "added successfully";
             return View(model);
         }
