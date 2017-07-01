e9ccfb8ff6c0cfa4177eb44e175dea9ccbb8dcab
Juggernaut <juggernaut.liu@gmail.com>
2017-07-01 19:09:39 +0800

    新增一個 BudgetController.Add() 的整合測試案例 for updated

--- GOOS_SampleTests/steps/BudgetCreateSteps.cs
+++ GOOS_SampleTests/steps/BudgetCreateSteps.cs
@@ -40,17 +40,17 @@ namespace GOOS_SampleTests.steps
             this._budgetCreatePage.ShouldDisplay(message);
         }
 
-        [Given(@"Budget table existed budgets")]
-        public void GivenBudgetTableExistedBudgets(Table table)
-        {
+        //[Given(@"Budget table existed budgets")]
+        //public void GivenBudgetTableExistedBudgets(Table table)
+        //{
 
-            var budgets = table.CreateSet<Budgets>();
-            using (var dbcontext = new goosEntities())
-            {
-                dbcontext.Budgets.AddRange(budgets);
-                dbcontext.SaveChanges();
-            }
-        }
+        //    var budgets = table.CreateSet<Budgets>();
+        //    using (var dbcontext = new goosEntities())
+        //    {
+        //        dbcontext.Budgets.AddRange(budgets);
+        //        dbcontext.SaveChanges();
+        //    }
+        //}
 
     }
 }
---
+++ GOOS_SampleTests/steps/Common/InsertTable.cs
@@ -0,0 +1,27 @@
+﻿using GOOS_SampleTests.DataModelsForIntegrationTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace GOOS_SampleTests.steps.Common
+{
+    [Binding]
+    public sealed class InsertTable
+    {
+        [Given(@"Budget table existed budgets")]
+        public void GivenBudgetTableExistedBudgets(Table table)
+        {
+            //same with BudgetCreateSteps
+            var budgets = table.CreateSet<Budgets>();
+            using (var dbcontext = new goosEntities())
+            {
+                dbcontext.Budgets.AddRange(budgets);
+                dbcontext.SaveChanges();
+            }
+        }
+    }
+}
